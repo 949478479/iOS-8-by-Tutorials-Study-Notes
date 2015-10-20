e716751229fed01b71da45851c36f6f7de917432
从今以后 <949478479@qq.com>
2015-10-21 05:20:04 +0800

    注释掉了自定义过渡动画部分的代码.

--- CustomPresentation/CustomPresentation/SimpleTransitioningDelegate.swift
+++ CustomPresentation/CustomPresentation/SimpleTransitioningDelegate.swift
@@ -17,16 +17,18 @@ class SimpleTransitioningDelegate: NSObject, UIViewControllerTransitioningDelega
             presentingViewController: presenting)
     }
 
-//    func animationControllerForPresentedController(presented: UIViewController,
-//        presentingController presenting: UIViewController,
-//        sourceController source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
-//        let animationController = SimpleTransitioner()
-//        animationController.isPresentation = true
-//        return animationController
-//    }
+/* 这些是自定义过渡动画的内容. UIPresentationController并不依赖于这些.
+    func animationControllerForPresentedController(presented: UIViewController,
+        presentingController presenting: UIViewController,
+        sourceController source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        let animationController = SimpleTransitioner()
+        animationController.isPresentation = true
+        return animationController
+    }
 
-//    func animationControllerForDismissedController(dismissed: UIViewController)
-//        -> UIViewControllerAnimatedTransitioning? {
-//        return SimpleTransitioner()
-//    }
+    func animationControllerForDismissedController(dismissed: UIViewController)
+        -> UIViewControllerAnimatedTransitioning? {
+        return SimpleTransitioner()
+    }
+*/
 }
